b8f572e6ef1f6c799f311b54e6c436d48773355d
Nitanshu Singh <nitanshusingh55@gmail.com>
2026-01-04 23:57:14 +0530

    adding source code

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00f52dff', // Change this to your primary color
+    },
+    secondary: {
+      main: '#00ba44ff', // Change this to your secondary color
+    },
+  },
+});
+
+export default theme;
