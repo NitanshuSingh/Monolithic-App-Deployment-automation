c85450358e376647331b9facd045fc1955dfbab1
Nitanshu Singh <166264908+NitanshuSingh@users.noreply.github.com>
2026-01-11 23:41:14 +0530

    Serve files from the 'build' directory

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (_, res) =>
+  res.sendFile(path.join(__dirname, 'build'))
+);
+
+app.listen(process.env.PORT || 3000);
